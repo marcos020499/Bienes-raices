b13c22f4b054c6521a72d356cbc076d0d2880497
validName <>
2021-05-05 08:26:12 -0500

    Updates

---
+++ precache-manifest.9bf9f51537c3f5cdbf44ab9f8d9d81c9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "c7c43448b9e24b470e80",
+    "url": "/Bienes-raices/static/css/main.c69e6a08.chunk.css"
+  },
+  {
+    "revision": "c7c43448b9e24b470e80",
+    "url": "/Bienes-raices/static/js/main.c4e1500c.chunk.js"
+  },
+  {
+    "revision": "9c0173946fde55384fa7",
+    "url": "/Bienes-raices/static/js/runtime~main.1009bc99.js"
+  },
+  {
+    "revision": "271a3697d2270f04741d",
+    "url": "/Bienes-raices/static/js/2.497f6254.chunk.js"
+  },
+  {
+    "revision": "74388ea0f92e268e5602c9f02b88b553",
+    "url": "/Bienes-raices/static/media/logo.74388ea0.svg"
+  },
+  {
+    "revision": "ecef733a33ceec8dc32d29510998afe9",
+    "url": "/Bienes-raices/static/media/loupe.ecef733a.svg"
+  },
+  {
+    "revision": "e98504c81b400b3c53926edae68c70a9",
+    "url": "/Bienes-raices/static/media/ecommerce-product-page-design-tubik-blog.e98504c8.jpg"
+  },
+  {
+    "revision": "a97beacfc7d7e10a350f64de1244c0c0",
+    "url": "/Bienes-raices/static/media/easter-bunny-adventures-bear-illustration-tubikarts-768x576.a97beacf.jpg"
+  },
+  {
+    "revision": "225080a3f598250308fb65956ba70090",
+    "url": "/Bienes-raices/static/media/breadcrumbs-design-tips-tubik-blog-768x576.225080a3.jpg"
+  },
+  {
+    "revision": "d46e3c8a850a6affdc07b8c5f9a08d28",
+    "url": "/Bienes-raices/static/media/creative-workspaces-illustrations-tubikarts-3-768x576.d46e3c8a.jpg"
+  },
+  {
+    "revision": "f2939fa80a59fd1f8485a9903c7393d1",
+    "url": "/Bienes-raices/static/media/photos-in-ui-design-tubik-blog-article-768x576.f2939fa8.jpg"
+  },
+  {
+    "revision": "1980187eb3f9a1bd97e484943d7d61e8",
+    "url": "/Bienes-raices/static/media/color-theory-design-tubik.1980187e.jpg"
+  },
+  {
+    "revision": "fd955984fcb72e4b3aefbeb1f2195f90",
+    "url": "/Bienes-raices/static/media/images-in-user-interfaces-tubik-blog-article.fd955984.png"
+  },
+  {
+    "revision": "8969efef2bd4f7d2e88f77366aaf99c5",
+    "url": "/Bienes-raices/static/media/cinema-app-ui-design-tubik.8969efef.jpg"
+  },
+  {
+    "revision": "6090d256d88dcd7f0244eaa4a3eafbba",
+    "url": "/Bienes-raices/static/media/Roboto-Light.6090d256.ttf"
+  },
+  {
+    "revision": "9ece5b48963bbc96309220952cda38aa",
+    "url": "/Bienes-raices/static/media/Roboto-Bold.9ece5b48.ttf"
+  },
+  {
+    "revision": "90ee8862950150162af077aff933301a",
+    "url": "/Bienes-raices/index.html"
+  }
+];
